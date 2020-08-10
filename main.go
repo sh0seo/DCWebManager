4e19965244e32810af29a409b2919b5aa4140dd7
godotlog <ssh0702@gmail.com>
2020-08-10 17:44:38 +0900

    update

--- main.go
+++ main.go
@@ -28,13 +28,15 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("[%7s] /", r.Method)
+		w.Header().Set("Content-Type", "text/html")
 
 		tmplIndex := "templates/index.html"
+		tmplLayout := "templates/include/layout.html"
 		tmplHeader := "templates/include/header.html"
 		tmplFooter := "templates/include/footer.html"
 		tmplSide := "templates/include/side.html"
 
-		tmpl := template.Must(template.ParseFiles(tmplIndex, tmplHeader, tmplFooter, tmplSide))
+		tmpl := template.Must(template.ParseFiles(tmplIndex, tmplLayout, tmplHeader, tmplFooter, tmplSide))
 		params := struct {
 			Title    string
 			PageName string
@@ -44,7 +46,7 @@ func main() {
 			PageName: "index",
 			UserName: "Teseter",
 		}
-		err := tmpl.ExecuteTemplate(w, "main", params)
+		err := tmpl.ExecuteTemplate(w, "index", params)
 		if err != nil {
 			http.Error(w, "405 Method Not Allowed", http.StatusMethodNotAllowed)
 			log.Print(err)
@@ -53,6 +55,8 @@ func main() {
 
 	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("[%7s] /login", r.Method)
+		w.Header().Set("Content-Type", "text/html"
+
 		if r.Method != "POST" {
 			http.Error(w, "405 Method Not Allowed", http.StatusMethodNotAllowed)
 			return
@@ -64,6 +68,8 @@ func main() {
 
 	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("[%7s] /logout", r.Method)
+		w.Header().Set("Content-Type", "text/html"
+
 		fmt.Fprintf(w, `{result":"success","code":200}`)
 	})
 
